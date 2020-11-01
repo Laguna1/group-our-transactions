5c8bc0abb5e0d833121a0c63fc3874452c93a1a3
Laguna1 <lagunazoomarket@gmail.com>
2020-11-02 01:41:59 +0200

    fix show views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.json
   def index
-    @groups = Group.all.order('name DESC')
+    @groups = Group.all.order('name ASC')
     @transactions = Transaction.all
   end
 
