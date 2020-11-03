eab3fcb47217af78052d92008c0d69d625184d86
Laguna1 <lagunazoomarket@gmail.com>
2020-11-04 01:31:09 +0200

    add groups validation test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Group, type: :model do
   describe 'validations to test' do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(20) }
+    it { should validate_presence_of(:icon) }
   end
   describe 'associations to test' do
     it { should belong_to(:user).required }
