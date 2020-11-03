53a522989f68818204db9a2e3f7be5c6f887770d
Laguna1 <lagunazoomarket@gmail.com>
2020-11-03 16:48:25 +0200

    finish external transactions view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def external
     user = User.find_by(id: current_user.id)
-    @external_user_transactions = user.transactions.where(group_id: nil).order(created_at: :desc)
-    @external_total = @external_user_transactions.sum(:amount)
+    @external_transactions = user.transactions.where(group_id: nil).order(created_at: :desc)
+    @external_total = @external_transactions.sum(:amount)
   end
 end
