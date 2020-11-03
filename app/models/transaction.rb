d4e3086e7a485cded66840ace5134100b8aa40ab
Laguna1 <lagunazoomarket@gmail.com>
2020-11-03 03:18:36 +0200

    add associations and validation tasts for Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :group, optional: true
   belongs_to :user
 
-  has_many :groups
-
   validates :name, :amount, presence: true
 
   def total_transaction
