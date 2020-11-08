725e3f64fb431b55071acbd3b08c50356de167df
Laguna1 <lagunazoomarket@gmail.com>
2020-11-09 01:48:10 +0200

    fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,9 @@
 module ApplicationHelper
-
   def nav_profile
     render partial: 'layouts/profile' if logged_in?
   end
 
   def current_user_destroy(group)
-    if group.user_id == current_user.id
-      link_to 'Delete', group, method: :delete, data: { confirm: 'Are you sure?' }
-    end
+    link_to 'Delete', group, method: :delete, data: { confirm: 'Are you sure?' } if group.user_id == current_user.id
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,6 @@ class Transaction < ApplicationRecord
   belongs_to :user
   has_many :groups
 
-
   validates :name, :amount, presence: true
 
   def total_transaction
